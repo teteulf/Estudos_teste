ddf1e51cc5d9f43dbbea071ff0af469cd261d965
Renan.T <rtoniolo4@gmail.com>
2023-07-25 20:11:01 -0300

    ajust renanzin ;)

--- arrays/testerenanzin.js
+++ arrays/testerenanzin.js
@@ -2,9 +2,16 @@ const title = document.querySelector(".tituloDesenhoFavorito");
 const botao = document.querySelector(".botao");
 const texto = document.querySelector(".instrucoesTexto");
 const main = document.querySelector(".instrucoes");
-let parag = document.querySelectorAll("p");
+let parag = document.querySelectorAll("img");
 
-let desenhos = [1, 2, 3, 4, 5, 6, 7];
+let desenhos = [
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/female/3.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/3.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/3.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/3.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/629.png",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/629.png",
+];
 let desenhosSelecionados = [];
 
 botao.addEventListener("click", function () {
@@ -17,24 +24,34 @@ botao.addEventListener("click", function () {
 });
 
 parag.forEach((elementParag) => {
-  elementParag.addEventListener("click", (element) => removeAndAddImg(element));
+  elementParag.addEventListener("click", (element) => {
+    removeAndAddImg(element);
+  });
 });
 
 function apareceImagem() {
-  let parag = document.querySelectorAll("p");
+  let parag = document.querySelectorAll("img");
   let firstImg = desenhos[0];
   let lastImg = desenhos[desenhos.length - 1];
-  parag[0].innerText = firstImg;
-  parag[1].innerText = lastImg;
+  parag[0].src = firstImg;
+  parag[1].src = lastImg;
 }
 
 function removeAndAddImg(element) {
   let firstImg = desenhos[0];
   let lastImg = desenhos[desenhos.length - 1];
+  let elementImg = element.target.src;
 
-  let elementImg = element.target.innerText;
   desenhos.splice(desenhos.indexOf(firstImg), 1);
   desenhos.splice(desenhos.indexOf(lastImg), 1);
   desenhosSelecionados.push(elementImg);
+
+  if (desenhos.length < 3) {
+    desenhos = desenhos.concat(desenhosSelecionados);
+    desenhosSelecionados = [];
+  }
   apareceImagem();
+
+  console.log(desenhos);
+  console.log(desenhosSelecionados);
 }
